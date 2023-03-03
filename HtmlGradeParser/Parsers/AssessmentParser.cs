f40ffb48e08485473451c8e8eb7e881a434e75e5
BmKo <60381441+BmKo@users.noreply.github.com>
2023-03-03 20:12:23 +1300

    Get correct grade and improve calculation

--- HtmlGradeParser/Parsers/AssessmentParser.cs
+++ HtmlGradeParser/Parsers/AssessmentParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Net;
+using System.Text.Json.Nodes;
 using HtmlAgilityPack;
 using HtmlGradeParser.Parsers.Interfaces;
 using HtmlGradeParser.Services;
@@ -9,31 +10,45 @@ public abstract class AssessmentParser : IParser
 {
     public static JsonNode Parse(HtmlNode node)
     {
-        var div = node.SelectSingleNode("div");
-        var header = div.SelectSingleNode("h4");
-
+        var header = node.SelectSingleNode("div/h4");
         var name = header?.SelectSingleNode("text()")?.InnerText.Trim();
-        var finalMark = header?.SelectSingleNode("span/text()")?
-            .InnerText.Split(':').LastOrDefault()?.Trim();
 
-        string? letterGrade = null;
+        // TODO implement questions and check valid size
+        var table = node.SelectSingleNode("div/div/table");
+        var questions = table.Descendants("tr");
+        var finalGrade = questions.LastOrDefault();
 
-        if (float.TryParse(finalMark ?? "", out var numberGrade))
-        {
-            letterGrade = GradeConverterService.ConvertToLetter(numberGrade);
-        }
-        else if (finalMark != null)
-        {
-            letterGrade = finalMark;
-            numberGrade = GradeConverterService.ConvertToNumber(finalMark);
-        }
+        var grades = GradeParser(finalGrade);
 
         return new JsonObject
         {
-            ["id"] = div.Id,
             ["name"] = name ?? "",
-            ["letter_grade"] = letterGrade,
-            ["number_grade"] = numberGrade
+            ["letter_grade"] = grades.Item1,
+            ["number_grade"] = grades.Item2
         };
+    }
+
+    private static (string, float) GradeParser(HtmlNode node)
+    {
+        var grade = WebUtility.HtmlDecode(node.SelectSingleNode("td/span/text()").InnerText);
+
+        if (grade.Contains('/'))
+        {
+            var split = grade.Split('/').Select(x => x.Trim()).ToArray();
+            if (split.Length != 2 || !float.TryParse(split[0], out var left) ||
+                !float.TryParse(split[1], out var right))
+            {
+                throw new ArgumentException($"Invalid grade format: {grade}", nameof(node));
+            }
+
+            var numberGrade = MathF.Round(left / right * 100, 2);
+
+            return (GradeConverterService.ConvertToLetter(numberGrade), numberGrade);
+        }
+        else
+        {
+            var numberGrade = GradeConverterService.ConvertToNumber(grade);
+            return (grade, numberGrade);
+        }
     }
 }
